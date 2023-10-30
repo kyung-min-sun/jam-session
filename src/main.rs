98b7669e28bbac3f69c9f4f0ad9b6cb053c3f943
kevinsun127 <kevinsun@usc.edu>
2023-10-29 21:49:18 -0700

    rust learning

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ fn main() {
     println!("Hello, world!");
     let help = yo();
     println!("{help} this is pretty cool omg");
-    println!("testing")
+    println!("testing");
+    let value = function_that_returns_things();
+    println!("{value}")
+}
+
+fn function_that_returns_things() -> i32 {
+    return 2;
 }
 
 fn yo() -> &'static str {
